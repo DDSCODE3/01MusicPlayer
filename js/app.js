05f52669ee5e331e166a556f41998782405bf319
Ruhollah-coding <ruhollahemail70@gmail.com>
2025-08-07 00:49:09 +0330

    first commit

---
+++ js/app.js
@@ -0,0 +1,77 @@
+const music = document.querySelector("audio");
+const play = document.querySelector(".play");
+const playAndPauseBtn = document.querySelector(".fa-play");
+const Forward = document.querySelector(".forward");
+const backForward = document.querySelector(".back-forward");
+const tenSecBackForwardBTN = document.querySelector(".ten-sec-back-forward");
+const tenSecforwardBTN = document.querySelector(".ten-sec-forward");
+const musicInformation = document.querySelector(".music_information");
+const musicCover = document.querySelector(".music_cover");
+
+let musicCounter = 0;
+const musics = [
+  {
+    id: 1,
+    src: "./public/musics/bende-naf-ta-khate-saf.mp3",
+    coverSrc: "public/cover1.jpg",
+    title: "بند ناف تا قاف",
+    artist: "یاس",
+  },
+  {
+    id: 2,
+    src: "./public/musics/sarkoob.mp3",
+    coverSrc: "public/cover2.jpg",
+    title: "سرکوب",
+    artist: "یاس",
+  },
+  {
+    id: 3,
+    src: "./public/musics/sefareshi.mp3",
+    coverSrc: "public/cover3.jpg",
+    title: "سفارشی",
+    artist: "یاس",
+  },
+];
+
+let isPlay = false;
+const randomIndex = Math.floor(Math.random()* musics.length)
+
+function playAndPause() {
+  if (isPlay) {
+    music.pause();
+    playAndPauseBtn.classList.add("fa-play");
+    playAndPauseBtn.classList.remove("fa-pause");
+    isPlay = false;
+  } else {
+    music.play();
+    playAndPauseBtn.classList.remove("fa-play");
+    playAndPauseBtn.classList.add("fa-pause");
+    isPlay = true;
+  }
+}
+function tenSecforward() {
+  music.currentTime += 10;
+}
+function tenSecBackforward() {
+  music.currentTime -= 10;
+}
+function forwardNextSong() {
+  musicCounter = (musicCounter + 1) % musics.length;
+  music.src = musics[musicCounter].src;
+  musicInformation.innerHTML = `${musics[musicCounter].title} - ${musics[musicCounter].artist}`;
+  musicCover.src = musics[musicCounter].coverSrc;
+}
+function forwardBacktSong() {
+  if (musicCounter >= 1) {
+    musicCounter = (musicCounter - 1) % musics.length;
+    music.src = musics[musicCounter].src;
+    musicInformation.innerHTML = `${musics[musicCounter].title} - ${musics[musicCounter].artist}`;
+    musicCover.src = musics[musicCounter].coverSrc;
+  }
+}
+
+play.addEventListener("click", playAndPause);
+tenSecforwardBTN.addEventListener("click", tenSecforward);
+tenSecBackForwardBTN.addEventListener("click", tenSecBackforward);
+Forward.addEventListener("click", forwardNextSong);
+backForward.addEventListener("click", forwardBacktSong);
